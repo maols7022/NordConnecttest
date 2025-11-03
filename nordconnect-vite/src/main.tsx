fd902a3b01be3bdacbb587d616958907b323f2bd
maols7022 <maols7022@student.nord.no>
2025-11-03 16:04:12 +0100

    Update main.tsx

--- nordconnect-vite/src/main.tsx
+++ nordconnect-vite/src/main.tsx
@@ -1,4 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><App /></React.StrictMode>)
+
+// Eventuell global styling (Tailwind lastes via CDN i index.html)
+// Hvis du senere legger til tailwind.config, kan du ha: import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
