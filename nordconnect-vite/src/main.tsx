d0a9a4054e728271fa06800abff8faaf2aeab524
maols7022 <maols7022@student.nord.no>
2025-11-03 16:35:20 +0100

    Update main.tsx

--- nordconnect-vite/src/main.tsx
+++ nordconnect-vite/src/main.tsx
@@ -1,12 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-// Eventuell global styling (Tailwind lastes via CDN i index.html)
-// Hvis du senere legger til tailwind.config, kan du ha: import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+// Vite kobler appen til <div id="root"></div> i index.html
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
