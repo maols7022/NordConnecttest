952fed618a85d339e1584c913c7b989ab62cb80b
maols7022 <maols7022@student.nord.no>
2025-11-24 21:49:31 +0100

    Update routes for demo pages in main.tsx

--- nordconnect-vite/src/main.tsx
+++ nordconnect-vite/src/main.tsx
@@ -4,8 +4,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import NordConnect from "./NordConnect";
 import RoomPage from "./pages/RoomPage";
-import VideoDemoPage from "./pages/VideoDemoPage";
-import BreakoutDemoPage from "./pages/BreakoutDemoPage";
+import VideoDemoPage from "./pages/VideoDemoPage"; // Quizkveld
+import StudyGroupDemoPage from "./pages/StudyGroupDemoPage"; // Studiegruppe
+import BreakoutDemoPage from "./pages/BreakoutDemoPage"; // Breakout-rom
 
 import "./index.css";
 
@@ -19,8 +20,9 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
         {/* Rom-visning som egen side, f.eks. /room/kaffe */}
         <Route path="/room/:id" element={<RoomPage />} />
 
-        {/* Test / demo-sider */}
-        <Route path="/video-demo" element={<VideoDemoPage />} />
+        {/* Demo-sider */}
+        <Route path="/quiz-demo" element={<VideoDemoPage />} />
+        <Route path="/study-demo" element={<StudyGroupDemoPage />} />
         <Route path="/breakout-demo" element={<BreakoutDemoPage />} />
       </Routes>
     </BrowserRouter>
