09581bee96f50f1b825a9945d1c29314345cd6b1
maols7022 <maols7022@student.nord.no>
2025-11-24 21:17:28 +0100

    Update main.tsx

--- nordconnect-vite/src/main.tsx
+++ nordconnect-vite/src/main.tsx
@@ -1,17 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import App from "./App";
+
+import NordConnect from "./NordConnect";
 import RoomPage from "./pages/RoomPage";
 import VideoDemoPage from "./pages/VideoDemoPage";
 import BreakoutDemoPage from "./pages/BreakoutDemoPage";
+
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
+        {/* Forsiden / hovedprototypen */}
+        <Route path="/" element={<NordConnect />} />
+
+        {/* Rom-visning som egen side, f.eks. /room/kaffe */}
         <Route path="/room/:id" element={<RoomPage />} />
+
+        {/* Test / demo-sider */}
         <Route path="/video-demo" element={<VideoDemoPage />} />
         <Route path="/breakout-demo" element={<BreakoutDemoPage />} />
       </Routes>
