56289c366d48f74c01d53fad4022e6c737536529
maols7022 <maols7022@student.nord.no>
2025-11-24 10:56:14 +0100

    Add routing for RoomPage in main.tsx

--- nordconnect-vite/src/main.tsx
+++ nordconnect-vite/src/main.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
+import RoomPage from "./pages/RoomPage";
 
-// Vite kobler appen til <div id="root"></div> i index.html
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/room/:id" element={<RoomPage />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
