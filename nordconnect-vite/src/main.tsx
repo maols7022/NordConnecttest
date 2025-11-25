aa2c39da40db9cf435ea1758a81549d7d2480515
maols7022 <maols7022@student.nord.no>
2025-11-25 18:57:04 +0100

    Update main.tsx

--- nordconnect-vite/src/main.tsx
+++ nordconnect-vite/src/main.tsx
@@ -8,6 +8,7 @@ import VideoDemoPage from "./pages/VideoDemoPage"; // Quizkveld
 import StudyGroupDemoPage from "./pages/StudyGroupDemoPage"; // Studiegruppe
 import BreakoutDemoPage from "./pages/BreakoutDemoPage"; // Breakout-rom
 import HowItWorksPage from "./pages/HowItWorksPage";
+import GamificationDemoPage from "./pages/GamificationDemoPage"; // NY: Gamification-demo
 
 import "./index.css";
 
@@ -15,13 +16,14 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-  <Route path="/" element={<NordConnect />} />
-  <Route path="/room/:id" element={<RoomPage />} />
-  <Route path="/quiz-demo" element={<VideoDemoPage />} />
-  <Route path="/study-demo" element={<StudyGroupDemoPage />} />
-  <Route path="/breakout-demo" element={<BreakoutDemoPage />} />
-  <Route path="/how-it-works" element={<HowItWorksPage />} />
-</Routes>
+        <Route path="/" element={<NordConnect />} />
+        <Route path="/room/:id" element={<RoomPage />} />
+        <Route path="/quiz-demo" element={<VideoDemoPage />} />
+        <Route path="/study-demo" element={<StudyGroupDemoPage />} />
+        <Route path="/breakout-demo" element={<BreakoutDemoPage />} />
+        <Route path="/how-it-works" element={<HowItWorksPage />} />
+        <Route path="/gamification-demo" element={<GamificationDemoPage />} /> {/* NY */}
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
